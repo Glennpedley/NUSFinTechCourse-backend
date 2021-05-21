dd4710f11000969f11d3ef44861d20357a770cb3
Hero <glennpedley@gmail.com>
2021-05-21 17:49:11 +0800

    your message

---
+++ function.js
@@ -0,0 +1,15 @@
+var can_vote = function (age)
+if (age< 18) {
+    return "No";
+}
+else {
+    return "yes";
+}
+};
+
+function execute(fn, parameter){
+    return fn(parameter);
+}
+
+
+//define two files in folder main.js database.js.. good practice//
